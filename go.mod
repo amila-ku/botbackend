74736bfa5881f356f6c8909ef81b3b6e5d73242f
amilaku <amila.kumaranayaka@gmail.com>
2019-10-03 23:30:36 +0200

    fix build errors

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/amila-ku/botbackend
+
+go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/aws/aws-sdk-go v1.25.5
+	github.com/gorilla/mux v1.7.3
+	github.com/sirupsen/logrus v1.4.2
+	github.com/swaggo/http-swagger v0.0.0-20190614090009-c2865af9083e
+	github.com/swaggo/swag v1.6.3
+)
---
+++ pkg/entity/entity.go
@@ -0,0 +1,57 @@
+package entity
+
+const (
+	FACEBOOK_API = "https://graph.facebook.com/v2.6/me/messages?access_token=%s"
+	IMAGE        = "http://37.media.tumblr.com/e705e901302b5925ffb2bcf3cacb5bcd/tumblr_n6vxziSQD11slv6upo3_500.gif"
+)
+
+type Callback struct {
+	Object string `json:"object,omitempty"`
+	Entry  []struct {
+		ID        string      `json:"id,omitempty"`
+		Time      int         `json:"time,omitempty"`
+		Messaging []Messaging `json:"messaging,omitempty"`
+	} 						  `json:"entry,omitempty"`
+}
+
+type Messaging struct {
+	Sender    User    `json:"sender,omitempty"`
+	Recipient User    `json:"recipient,omitempty"`
+	Timestamp int     `json:"timestamp,omitempty"`
+	Message   Message `json:"message,omitempty"`
+}
+
+type User struct {
+	ID string `json:"id,omitempty"`
+}
+
+type Message struct {
+	MID        string `json:"mid,omitempty"`
+	Text       string `json:"text,omitempty"`
+	QuickReply *struct {
+		Payload string `json:"payload,omitempty"`
+	} `json:"quick_reply,omitempty"`
+	Attachments *[]Attachment `json:"attachments,omitempty"`
+	Attachment  *Attachment   `json:"attachment,omitempty"`
+}
+
+type Attachment struct {
+	Type    string  `json:"type,omitempty"`
+	Payload Payload `json:"payload,omitempty"`
+}
+
+type Response struct {
+	Recipient User    `json:"recipient,omitempty"`
+	Message   Message `json:"message,omitempty"`
+}
+
+type Payload struct {
+	URL string `json:"url,omitempty"`
+}
+
+// APIError is to record errors
+type APIError struct {
+	ErrorCode    int
+	ErrorMessage string
+}
+
