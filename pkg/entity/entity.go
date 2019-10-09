7b3120022ae4af9d018fef78f749084035362600
amilaku <amila.kumaranayaka@gmail.com>
2019-10-09 23:13:23 +0200

    remove unsued const

--- pkg/entity/entity.go
+++ pkg/entity/entity.go
@@ -2,7 +2,6 @@ package entity
 
 const (
 	FACEBOOK_API = "https://graph.facebook.com/v2.6/me/messages?access_token=%s"
-	IMAGE        = "http://37.media.tumblr.com/e705e901302b5925ffb2bcf3cacb5bcd/tumblr_n6vxziSQD11slv6upo3_500.gif"
 )
 
 type Callback struct {
@@ -11,7 +10,7 @@ type Callback struct {
 		ID        string      `json:"id,omitempty"`
 		Time      int         `json:"time,omitempty"`
 		Messaging []Messaging `json:"messaging,omitempty"`
-	} 						  `json:"entry,omitempty"`
+	} `json:"entry,omitempty"`
 }
 
 type Messaging struct {
@@ -54,4 +53,3 @@ type APIError struct {
 	ErrorCode    int
 	ErrorMessage string
 }
-
